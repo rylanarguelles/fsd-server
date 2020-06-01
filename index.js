833817bf4438669592edff181d27df96f0a2d3c9
slick <62088941+taravannheng@users.noreply.github.com>
2020-06-01 13:06:21 +1000

    Update Task 1

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ pool = new Pool({
 
 // Task 1
 
+router.get('/menu', async ctx => {
+    const { rows } = await pool.query('SELECT * FROM menu_item');
+    ctx.body = rows;
+});
+
 // Task 2
 
 //
